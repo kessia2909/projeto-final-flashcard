27450bcb1e7aba225c3ad27d94fd0802ef0814c6
kessia2909 <00001083552570sp@al.educacao.sp.gov.br>
2024-11-13 08:44:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Portugues',
+    'O que é Futurismo?',
+    'Futurismo é escola literária do modernismo'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais fenomenos são causados pelo encontro de placas tectônicos?',
+    'O encontro de placas causa terremotos e furacões'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz talvez em ingles?',
+    'Uma das formas de fala talvez é parhaps'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz melhor amigo em ingles?',
+    'Melhor amigo se diz best friend'
+)
